c9937426557118695d0aa969aa10e1270f255313
Артур Сержин <49846937+Evcofo@users.noreply.github.com>
2019-05-11 20:06:22 +0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 const VkBot = require('node-vk-bot-api') // npm install node-vk-bot-api --save						
 const bot = new VkBot({
-  token: '2131212132121213', // ТОКЕН ГРУППЫ
+  token: '2131212132121213', // ТОКЕН  (ИЗ НАСТРОЕК В ГРУППЕ) ГРУППЫ
   group_id: 123132131,	// ID ГРУППЫ ВК
   execute_timeout: 50, // in ms   (50 by default)
   polling_timeout: 25, // in secs (25 by default)
